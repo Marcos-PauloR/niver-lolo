eb8752ecf946ff03db56a4a20bb6586586ab5f9c
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2024-10-10 15:54:10 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host:true
-  },
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_URL_BACK, // URL do seu servidor backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove o prefixo '/api'
+      },
+    },
+  },
 })
